a6f6ade87c233e5c0764f7f42803ac5f185d6708
Stephanie Reinders <srein@iastate.edu>
2020-06-22 11:36:59 -0500

    change getDataForTag to take url as input instead of read_html(url)

--- R/getDataForTag.R
+++ R/getDataForTag.R
@@ -2,24 +2,26 @@
 #'
 #' Scrape an app page in the Google Play Store for information associated with a specific html tag.
 #'
-#' @param webpage The html for an app page in the Google Play Store
+#' @param url The url for an app page in the Google Play Store
 #' @param html_tag An html tag. E.g. "div"
 #' @param html_attribute An html attribute associated with the html tag. E.g. "class"
 #' @param html_attribute_value The value associated with the html attribute. E.g. "KmO8jd"
+#'
+#' @import rvest
+#' @import xml2
 #'
 #' @return
 #' @export
 #'
 #' @examples
-#' webpage <- read_html("https://play.google.com/store/apps/details?id=com.dinaga.photosecret")
-#' getDataForTag(webpage,"div","class","KmO8jd")
-#' getDataForTag(webpage,"a","class","hrTbp R8zArc")
+#' getDataForTag("https://play.google.com/store/apps/details?id=com.dinaga.photosecret","div","class","KmO8jd")
+#' getDataForTag("https://play.google.com/store/apps/details?id=com.dinaga.photosecret","a","class","hrTbp R8zArc")
 
-getDataForTag <- function(webpage, html_tag, html_attribute, html_attribute_value) {
+getDataForTag <- function(url, html_tag, html_attribute, html_attribute_value) {
 
   search_text <- paste0("//", html_tag, "[contains(@", html_attribute, ",'", html_attribute_value, "')]")
 
-  data <- webpage %>%
+  data <- url %>%
     rvest::html_nodes('body') %>%
     xml2::xml_find_all(search_text) %>%
     rvest::html_text()
