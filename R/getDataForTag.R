5c39ed9490b2c15073c5c3458fffb5617dfdf869
Stephanie Reinders <srein@iastate.edu>
2020-06-22 11:24:04 -0500

    add ROxygen tags to getDataForTag

--- R/getDataForTag.R
+++ R/getDataForTag.R
@@ -1,3 +1,20 @@
+#' getDataForTag
+#'
+#' Scrape an app page in the Google Play Store for information associated with a specific html tag.
+#'
+#' @param webpage The html for an app page in the Google Play Store
+#' @param html_tag An html tag. E.g. "div"
+#' @param html_attribute An html attribute associated with the html tag. E.g. "class"
+#' @param html_attribute_value The value associated with the html attribute. E.g. "KmO8jd"
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' webpage <- read_html("https://play.google.com/store/apps/details?id=com.dinaga.photosecret")
+#' getDataForTag(webpage,"div","class","KmO8jd")
+#' getDataForTag(webpage,"a","class","hrTbp R8zArc")
+
 getDataForTag <- function(webpage, html_tag, html_attribute, html_attribute_value) {
 
   search_text <- paste0("//", html_tag, "[contains(@", html_attribute, ",'", html_attribute_value, "')]")
