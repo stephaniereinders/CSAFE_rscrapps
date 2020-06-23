8e14f985e95d68ca2526d6182f690f52589b2a54
Stephanie Reinders <srein@iastate.edu>
2020-06-23 14:39:05 -0500

    add more tests for getDataForApps

--- tests/testthat/test-getDataForApps.R
+++ tests/testthat/test-getDataForApps.R
@@ -1,4 +1,4 @@
-context("getDataForTag")
+context("getDataForApps")
 
 test_that("search term is a character", {
   expect_error(getDataForApps(50))
@@ -6,5 +6,13 @@ test_that("search term is a character", {
 
 test_that("search term is non-empty", {
   expect_error(getDataForApps(""))
+
+test_that("num_apps is not a positive integer and not 'all'", {
+    expect_error(getDataForApps("steganography", num_apps = 3.4))
+    expect_error(getDataForApps("steganography", num_apps = 0))
+    expect_error(getDataForApps("steganography", num_apps = "five"))
+    expect_error(getDataForApps("steganography", num_apps = "5"))
+  })
+
 })
 
